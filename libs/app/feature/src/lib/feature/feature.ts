93ba0e1ba2e39bb284e1c792694a1ea4fb15c57b
Banks, Kyle <kyle.ryan.banks@gmail.com>
2026-08-14 23:22:43 -0500

    feat: add feature library with routing, components, and testing setup

---
+++ libs/app/feature/src/lib/feature/feature.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Ui } from '@theory-fighter-network/ui';
+
+@Component({
+  selector: 'tfn-feature',
+  imports: [Ui],
+  templateUrl: './feature.html',
+  styleUrl: './feature.css',
+})
+export class Feature {}
